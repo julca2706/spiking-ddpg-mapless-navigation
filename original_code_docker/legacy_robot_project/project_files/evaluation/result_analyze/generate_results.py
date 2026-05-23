05bbecd8d559a0acdc2f2c117254346c82f65d46
julca2706 <juliacaluch@gmail.com>
2026-05-23 17:23:16 +0200

    last test

--- original_code_docker/legacy_robot_project/project_files/evaluation/result_analyze/generate_results.py
+++ original_code_docker/legacy_robot_project/project_files/evaluation/result_analyze/generate_results.py
@@ -6,8 +6,8 @@ sys.path.append('../../')
 from evaluation.result_analyze.utility import analyze_run, plot_robot_paths
 from evaluation.eval_random_simulation.utility import gen_test_env_poly_list_env
 
-MODEL_NAME = 'sddpg_bw_5'
-# MODEL_NAME = 'ddpg'
+# MODEL_NAME = 'sddpg_bw_5'
+MODEL_NAME = 'ddpg'
 # MODEL_NAME = 'ddpg_poisson'
 FILE_NAME = MODEL_NAME + '_0_199.p'
 
